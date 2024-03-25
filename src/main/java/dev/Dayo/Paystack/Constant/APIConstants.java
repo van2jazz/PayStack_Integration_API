307e5c6e3a656e42b19914c8ce20b886deb49011
Tess <dayobalogun221@gmail.com>
2024-03-25 14:40:36 +0100

    update

---
+++ src/main/java/dev/Dayo/Paystack/Constant/APIConstants.java
@@ -0,0 +1,15 @@
+package dev.Dayo.Paystack.Constant;
+
+public class APIConstants {
+
+    public static final Integer STATUS_CODE_OK = 200;
+
+    public static final Integer STATUS_CODE_CREATED = 201;
+
+    public static final String PAYSTACK_INIT = "https://api.paystack.co/plan";
+
+    public static final String PAYSTACK_INITIALIZE_PAY = "https://api.paystack.co/transaction/initialize";
+
+    public final String PAYSTACK_VERIFY = "https//api.paystack.co/transaction/verify/";
+
+}
